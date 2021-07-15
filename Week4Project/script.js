27e624f8a6e314455ebe9099e96ecfb99cc8e314
BrendenBee <Brenden.Brushaber@gmail.com>
2021-07-15 11:29:40 -0500

    Tip Calculator

--- Week4Project/script.js
+++ Week4Project/script.js
@@ -1,3 +1,105 @@
+let totalBill;
+function totalBillFunc() {
+    totalBill = document.getElementById('userInputBill').value;
+    console.log(totalBill);
+    return totalBill;
+};
+
+// let subButton = document.getElementById('subButton');
+// subButton.addEventListener('click', totalBillFunc, false);
+
+
+let orderQuality;
+function OrderQualityFunc() {
+    
+
+    if (document.getElementById('userInputQualityFair').checked === true) {
+        orderQuality = 1.10;
+    } else if (document.getElementById('userInputQualityGood').checked === true) {
+        orderQuality = 1.15;  
+    } else if (document.getElementById('userInputQualityAwesome').checked === true) {
+        orderQuality = 1.20;
+    } else {
+        orderQuality = 1.25;
+
+    };
+};
+
+    // this disables the on checked boxes (should have used radio instead...)
+$(':checkbox').change(function(){
+    // "this" is the current checkbox
+    $(':checkbox').not(this).prop('disabled', this.checked);
+});
+
+// };
+// function OrderQualityGoodFunc() {
+//     OrderQuality = document.getElementById('userInputQualityGood').value;
+//     OrderQuality = 1.10;
+//     console.log(userInputQuality);
+//     return userInputQuality;
+// };
+// function OrderQualityAwesomeFunc() {
+//     OrderQuality = document.getElementById('userInputQualityAwesome').value;
+//     OrderQuality = 1.15;
+//     console.log(userInputQuality);
+//     return userInputQuality;
+// };
+// function OrderQualityOutstandingFunc() {
+//     OrderQuality = document.getElementById('userInputQualityOutstandingOutstandng').value;
+//     OrderQuality = 1.20;
+//     console.log(userInputQuality);
+//     return userInputQuality;
+// };
+
+let partySize;
+function partySizeFunc() {
+    partySize = document.getElementById('userInputSize').value;
+    console.log(partySize);
+    return partySize;
+};
+let totalAfterTip;
+let totalTip;
+
+
+
+     function calculateTotalFunc() {
+        totalAfterTip = (totalBill * orderQuality) /  partySize;
+        totalAfterTip = Math.round(totalAfterTip * 100) / 100;
+        totalAfterTip = totalAfterTip.toFixed(2);
+        console.log(orderQuality);
+        console.log(totalAfterTip);
+        console.log(totalBill);
+        console.log(partySize);
+        document.getElementById("totalBillWithTip").placeholder = totalAfterTip;
+        return totalAfterTip;
+    }
+    
+    
+    
+    function totalTipFunc() {
+        totalTip = (orderQuality * totalBill - totalBill);
+        totalAfterTip = Math.round(totalAfterTip * 100) / 100;
+        totalTip = totalTip.toFixed(2);
+        console.log(totalTip);
+        document.getElementById("totalTip").placeholder = totalTip;
+        return totalTip;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -10,53 +112,53 @@
 //     console.log(userInput);
 //     return userInput;
 // }  
-let totalBill;
-function totalBillFunc() {
-    totalBill = document.getElementById('userInputBill').value;
-    console.log(totalBill);
-    return totalBill;
-};
-
-let subButton = document.getElementById('subButton');
-subButton.addEventListener('click', totalBillFunc, false);
-
-let totalBill;
-function totalBillFunc() {
-    totalBill = document.getElementById('userInputBill').value;
-    console.log(totalBill);
-    return totalBill;
-};
-
-//new line
-// console.log(totalBill());
-
-// let calculator = {
-//     add: function(a,b) {return a+b;
-//     },
+// let totalBill;
+// function totalBillFunc() {
+//     totalBill = document.getElementById('userInputBill').value;
+//     console.log(totalBill);
+//     return totalBill;
 // };
-// function (a,b){console.log("I don't exist!")};
-// //point is cleaner code scope management etc
-// console.log(calculator.add(1,2));
 
-// let totalBill = {
-//     input: function(userInput) {
-//         userInput = document.getElementById('userInputBill').value;
-//         return userInput;
-//     }
+// let subButton = document.getElementById('subButton');
+// subButton.addEventListener('click', totalBillFunc, false);
 
-// }
+// let totalBill;
+// function totalBillFunc() {
+//     totalBill = document.getElementById('userInputBill').value;
+//     console.log(totalBill);
+//     return totalBill;
+// };
 
-// console.log(totalBill.input(userInputBill));
+// //new line
+// // console.log(totalBill());
+
+// // let calculator = {
+// //     add: function(a,b) {return a+b;
+// //     },
+// // };
+// // function (a,b){console.log("I don't exist!")};
+// // //point is cleaner code scope management etc
+// // console.log(calculator.add(1,2));
+
+// // let totalBill = {
+// //     input: function(userInput) {
+// //         userInput = document.getElementById('userInputBill').value;
+// //         return userInput;
+// //     }
+
+// // }
+
+// // console.log(totalBill.input(userInputBill));
 
 
 
 
 
-function testFunc() {
-    let newValue = totalBill * 1.5;
-    console.log(newValue);
-    return newValue;
-};
+// function testFunc() {
+//     let newValue = totalBill * 1.5;
+//     console.log(newValue);
+//     return newValue;
+// };
 
 // let subButton2 = document.getElementById('subButton2');
 // subButton2.addEventListener('click', testFunc, false);
@@ -108,4 +210,4 @@ function testFunc() {
 
 
 // var subButton = document.getElementById('subButton');
-// subButton.addEventListener('click', getInput, false);
+// subButton.addEventListener('click', getInput, false)
